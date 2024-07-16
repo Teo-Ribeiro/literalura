2bb4d45b4a9b87f1fc90599ba4b1df8e0e23e3ad
Téo Ribeiro <90355181+Teo-Ribeiro@users.noreply.github.com>
2024-07-16 05:07:49 -0300

    Add files via upload

---
+++ src/main/java/br/com/alura/literalura/repository/LivroRepository.java
@@ -0,0 +1,15 @@
+package br.com.alura.literalura.repository;
+
+import br.com.alura.literalura.model.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface LivroRepository extends JpaRepository<Livro, Long> {
+
+    List<Livro> findByIdioma(String Idioma);
+
+    Integer countByIdioma(String Idioma);
+
+    List<Livro> findTop10ByOrderByNumeroDownloadsDesc();
+}
